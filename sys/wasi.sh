a88f9804f949a08a44c27a0b988fb7e886defcf3
pancake <pancake@nopcode.org>
2021-06-17 02:49:04 +0200

    more

--- sys/wasi.sh
+++ sys/wasi.sh
@@ -10,3 +10,8 @@ cp dist/plugins-cfg/plugins.wasi.cfg plugins.cfg
 # export CC="${WASI_SDK}/bin/clang -D
 ./configure --with-compiler=wasi --disable-debugger --without-fork --with-ostype=wasi --with-checks-level=0 --disable-threads --without-dylink --with-libr --without-libuv --without-gpl
 make -j
+make -C binr/rax2 wasi
+make -C binr/radare2 wasi
+make -C binr/rasm2 wasi
+make -C binr/rabin2 wasi
+make -C binr/rafind2 wasi
